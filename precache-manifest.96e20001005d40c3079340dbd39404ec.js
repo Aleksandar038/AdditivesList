a92154014c75ed505d6fce79dad847bff5c78cc7
Aleksandar Kukolj <kukoljaleksandar@hotmail.rs>
2020-03-21 01:33:04 +0100

    Updates

---
+++ precache-manifest.96e20001005d40c3079340dbd39404ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf2c33a2abc001292ef5e1f64940947",
+    "url": "/AdditiveChecker/index.html"
+  },
+  {
+    "revision": "a841ba85e6036ca7f939",
+    "url": "/AdditiveChecker/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e7054f3fb3e0ea0b6fa7",
+    "url": "/AdditiveChecker/static/css/main.ee8f572d.chunk.css"
+  },
+  {
+    "revision": "a841ba85e6036ca7f939",
+    "url": "/AdditiveChecker/static/js/2.799ccffc.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/AdditiveChecker/static/js/2.799ccffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7054f3fb3e0ea0b6fa7",
+    "url": "/AdditiveChecker/static/js/main.70642693.chunk.js"
+  },
+  {
+    "revision": "c9ff18fb82da61fae41e",
+    "url": "/AdditiveChecker/static/js/runtime-main.b4e449c8.js"
+  }
+]);
